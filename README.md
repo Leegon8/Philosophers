32558e17b3422779e9231d4687e9ecc0dc42b9cd
lauriago <lauriago@student.42barcelona.com>
2024-11-29 14:52:05 +0100

    sdfhjosfi

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # Philosophers
+
+Los filosofos --> comen, duermen y piensan. NO pueden hacer 2 cosas a la vez.
+
+NUM de tenedores = Num de filosofos
+
+Necesitan 2 tenedores para comer. Comen, (dejan los tenedores),  duermen y piensan.
--- src/check.c
+++ src/check.c
@@ -1,23 +1,15 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   philo.h                                            :+:      :+:    :+:   */
+/*   check.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: lauriago <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/11/28 15:50:26 by lauriago          #+#    #+#             */
-/*   Updated: 2024/11/29 14:35:23 by lauriago         ###   ########.fr       */
+/*   Created: 2024/11/29 14:32:47 by lauriago          #+#    #+#             */
+/*   Updated: 2024/11/29 14:51:51 by lauriago         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef PHILO_H
-# define PHILO_H
+#include "philo.h"
 
-# include <stdio.h>
-# include <unistd.h>
-# include <string.h>
-# include <stdlib.h>
-# include <sys/time.h>
-# include <pthread.h>
 
-#endif
